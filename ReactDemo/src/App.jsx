b5794f4880b90fcfd82c16886b3f78b5d0c7d99d
aasif-create <aasifmmd12345@gmail.com>
2026-06-26 23:27:06 +0530

    UPDATED

--- ReactDemo/src/App.jsx
+++ ReactDemo/src/App.jsx
@@ -7,15 +7,16 @@ import Projects from "./components/Projects";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
+import Top from "./components/Top"
 function App() {
   return (
     <>
+      <Top />
       <Navbar />
       <main>
-        <Hero />
         <About />
         <Skills />
+       <Hero />
         <Experience />
         <Projects />
         <Education />
---
+++ ReactDemo/src/components/Top.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Top.css"
+const Top = () => {
+  return (
+
+    <div className='blue'>Abubakar</div>
+  )
+}
+
+export default Top
